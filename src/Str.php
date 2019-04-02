cda4215e4adbcd7645659dd150ccb4d3ee84c6f9
liuml <723164417@qq.com>
2019-04-02 10:53:18 +0800

    add Str Class

---
+++ src/Str.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * String.php
+ *
+ * Created by PhpStorm.
+ *
+ * author: liuml  <liumenglei0211@163.com>
+ *
+ * DateTime: 2019-04-02  10:37
+ */
+
+namespace WannanBigPig\Supports;
+
+
+class Str
+{
+
+    /**
+     * This is a cache of case strings
+     * @var array
+     */
+    protected static $studlyCache = [];
+
+    /**
+     * static studly
+     *
+     * @param $value
+     *
+     * @return mixed
+     *
+     *
+     * @author   liuml  <liumenglei0211@163.com>
+     *
+     * @DateTime 2019-04-02  10:38
+     */
+    public static function studly($value)
+    {
+        $key = $value;
+
+        if (isset(static::$studlyCache[$key])) {
+            return static::$studlyCache[$key];
+        }
+
+        $value = ucwords(str_replace(['-', '_'], ' ', $value));
+
+        return static::$studlyCache[$key] = str_replace(' ', '', $value);
+    }
+}
