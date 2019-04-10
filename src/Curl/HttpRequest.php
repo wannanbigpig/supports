9998fc4740d6c8d3ac17aac7af3ef72afc50c387
liuml <723164417@qq.com>
2019-04-10 10:07:07 +0800

    edit HttpRequest

--- src/Curl/HttpRequest.php
+++ src/Curl/HttpRequest.php
@@ -151,13 +151,13 @@ trait HttpRequest
      */
     public function unwrapResponse(ResponseInterface $response)
     {
-        $contentType = $response->getHeaderLine('Content-Type');
+        // $contentType = $response->getHeaderLine('Content-Type');
         $contents    = $response->getBody()->getContents();
-        if (false !== stripos($contentType, 'json') || stripos($contentType, 'javascript')) {
+        /*if (false !== stripos($contentType, 'json') || stripos($contentType, 'javascript')) {
             return json_decode($contents, true);
         } elseif (false !== stripos($contentType, 'xml')) {
             return json_decode(json_encode(simplexml_load_string($contents, 'SimpleXMLElement', LIBXML_NOCDATA), JSON_UNESCAPED_UNICODE), true);
-        }
+        }*/
         return $contents;
     }
 }
